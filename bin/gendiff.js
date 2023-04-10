ed04404192e0d185a0395c53074905023c9486d6
Antipoop <senkovskiy.nikita@hexly.ru>
2023-04-10 12:57:50 +0300

    Fix last commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,5 @@ const  program  = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.01')
-.option('-f, --format <type>',  'output format');
+.option('-f, --format <type>', 'output format');
 program.parse();
