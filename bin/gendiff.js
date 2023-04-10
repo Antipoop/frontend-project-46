38ea4f0992ad7ef3ebcf26beede306e79b8bc52c
Antipoop <senkovskiy.nikita@hexly.ru>
2023-04-10 12:56:42 +0300

    Add option for commander gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,6 @@ const  program  = new Command();
 
 program
 .description('Compares two configuration files and shows a difference.')
-.version('0.01');
+.version('0.01')
+.option('-f, --format <type>',  'output format');
 program.parse();
