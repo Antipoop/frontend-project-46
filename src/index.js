28ee3b93ce3712797e1b5d2a3d4c50f76c928cb0
Antipoop <senkovskiy.nikita@hexly.ru>
2023-04-14 14:51:33 +0300

    better gendiff

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
 import path from 'path';
 import fs from 'fs';
 import process from 'process';
+import _ from 'lodash';
 
 const getAbsolutePath = (filename) => path.resolve(process.cwd(), filename);
 
 const genDiff = (filepath1, filepath2) => {
-  const data1 = JSON.parse(fs.readFileSync(getAbsolutePath(filepath1)));
-  const data2 = JSON.parse(fs.readFileSync(getAbsolutePath(filepath2)));
-  const keys2 = Object.entries(data2);
-  const keys1 = Object.entries(data1).sort();
-  console.log('{');
-  keys1.map((el) => {
-    if (data2[el[0]] !== undefined) {
-      if (data1[el[0]] === data2[el[0]]) {
-        const key = el[0];
-        const value = data1[el[0]];
-        console.log(`   ${key}: ${value}`);
+  const file1 = JSON.parse(fs.readFileSync(getAbsolutePath(filepath1)));
+  const file2 = JSON.parse(fs.readFileSync(getAbsolutePath(filepath2)));
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
+  const all = _.sortBy(_.union(keys1, keys2));
+  const iter = (arr1, arr2) => {
+    let result = '{\n';
+    all.map((key) => {
+      if (arr1.includes(key) && arr2.includes(key)) {
+        if (file1[key] === file2[key]) {
+          result += `   ${key}: ${file1[key]} \n`;
+        } else {
+          result += ` - ${key}: ${file1[key]}\n + ${key}: ${file2[key]}\n`;
+        }
+      } else if (arr1.includes(key)) {
+        result += ` - ${key}: ${file1[key]}\n`;
+      } else if (arr2.includes(key)) {
+        result += ` + ${key}: ${file2[key]}\n`;
       }
-    }
-    if (data2[el[0]] === undefined) {
-      console.log(` - ${el[0]}: ${data1[el[0]]}`);
-    }
-    if (data1[el[0]] !== data2[el[0]] && !!data2[el[0]]) {
-      console.log(` - ${el[0]}: ${data1[el[0]]}
- + ${el[0]}: ${data2[el[0]]}`);
-    }
-  });
-  keys2.map((el) => {
-    if (data1[el[0]] === undefined) {
-      console.log(` + ${el[0]}: ${data2[el[0]]}`);
-    }
-  });
-  console.log('}');
+      return result;
+    });
+    result += '}';
+    return result;
+  };
+  console.log(iter(keys1, keys2));
 };
 
 export default genDiff;
