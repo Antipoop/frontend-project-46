b2abb66ec11fa7d62f847647ec279b74aa56ddf3
Antipoop <senkovskiy.nikita@hexly.ru>
2023-04-14 11:11:02 +0300

    fix eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { genDiff } from '../src/index.js';
+import genDiff from '../src/index.js';
 
-const  program  = new Command();
+const program = new Command();
 
 program
-.description('Compares two configuration files and shows a difference.')
-.arguments('<filepath1> <filepath2>')
-.version('0.01')
-.option('-f, --format <type>', 'output format')
-.action((filepath1, filepath2) => {
-    genDiff(filepath1, filepath2)
-});
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .version('0.01')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    genDiff(filepath1, filepath2);
+  });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 import path from 'path';
 import fs from 'fs';
+import process from 'process';
 
-const getAbsolutePath = (filename) => path.resolve(process.cwd(), filename)
+const getAbsolutePath = (filename) => path.resolve(process.cwd(), filename);
 
 const genDiff = (filepath1, filepath2) => {
-const data1 = JSON.parse(fs.readFileSync(getAbsolutePath(filepath1)))
-const data2 = JSON.parse(fs.readFileSync(getAbsolutePath(filepath2))) 
-const keys2 = Object.entries(data2)
-const keys1 = Object.entries(data1).sort()
-console.log('{')
-keys1.map((el) => {
+  const data1 = JSON.parse(fs.readFileSync(getAbsolutePath(filepath1)));
+  const data2 = JSON.parse(fs.readFileSync(getAbsolutePath(filepath2)));
+  const keys2 = Object.entries(data2);
+  const keys1 = Object.entries(data1).sort();
+  console.log('{');
+  keys1.map((el) => {
     if (data2[el[0]] !== undefined) {
-        if (data1[el[0]] === data2[el[0]]) {
-            const key = el[0];
-            const value = data1[el[0]]
-            console.log(`   ${key}: ${value}`)
-        }
+      if (data1[el[0]] === data2[el[0]]) {
+        const key = el[0];
+        const value = data1[el[0]];
+        console.log(`   ${key}: ${value}`);
+      }
     }
-    if (data2[el[0]] === undefined){
-        console.log(` - ${el[0]}: ${data1[el[0]]}`)
+    if (data2[el[0]] === undefined) {
+      console.log(` - ${el[0]}: ${data1[el[0]]}`);
     }
-    if (data1[el[0]] !== data2[el[0]] && !!data2[el[0]]){
-        console.log(` - ${el[0]}: ${data1[el[0]]}
- + ${el[0]}: ${data2[el[0]]}`)
+    if (data1[el[0]] !== data2[el[0]] && !!data2[el[0]]) {
+      console.log(` - ${el[0]}: ${data1[el[0]]}
+ + ${el[0]}: ${data2[el[0]]}`);
     }
-}
-)
-keys2.map((el) => {
+  });
+  keys2.map((el) => {
     if (data1[el[0]] === undefined) {
-        console.log(` + ${el[0]}: ${data2[el[0]]}`)
+      console.log(` + ${el[0]}: ${data2[el[0]]}`);
     }
-})
-console.log('}')
-}
+  });
+  console.log('}');
+};
 
-export { genDiff }
+export default genDiff;
